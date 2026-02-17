ba0785fcf352809e60ffd7cb47ec433e01e899ab
joelmolina54 <joelmolina5463@gmail.com>
2026-02-17 18:33:35 -0500

    use railway mysql url

--- src/config/db.js
+++ src/config/db.js
@@ -7,10 +7,10 @@ const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
-    console.error('❌ Error de conexión a la base de datos:', err.message);
+    console.error('Error de conexión:', err.message);
     return;
   }
-  console.log('✅ Conectado a MySQL en Railway');
+  console.log('Conectado a MySQL en Railway');
 });
 
 export default db;
