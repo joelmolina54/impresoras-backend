868a09b8340a13daa4d77cfb0a75db2427670d2e
joelmolina54 <joelmolina5463@gmail.com>
2026-02-17 17:54:38 -0500

    conexion a mysql railway

--- src/config/db.js
+++ src/config/db.js
@@ -3,19 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS || '', // En caso de que esté vacía
-  database: process.env.DB_NAME,
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
     console.error('❌ Error de conexión a la base de datos:', err.message);
     return;
   }
-  console.log('✅ Conectado a la base de datos MySQL');
+  console.log('✅ Conectado a MySQL en Railway');
 });
 
 export default db;
